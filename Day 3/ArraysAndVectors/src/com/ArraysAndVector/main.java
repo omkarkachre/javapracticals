f5627760620e31ff3f1d378658c5de85534030a4
omkarkachre <110754398+omkarkachre@users.noreply.github.com>
2022-08-28 08:54:13 +0530

    Add files via upload

---
+++ Day 3/ArraysAndVectors/src/com/ArraysAndVector/main.java
@@ -0,0 +1,31 @@
+package com.ArraysAndVector;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enter the size of array:");
+		Scanner userinputObj = new Scanner(System.in);
+		
+		
+		int userinput = userinputObj.nextInt();
+		ArraysExample aa = new ArraysExample(userinput);
+		
+		// Getting the array size dynamically
+		int size = aa.student_rollno.length;
+		System.out.println("Size of array is: " + size);
+		
+		//Setting up the values of the array 
+		for (int i = 0; i < size; i++) {
+			aa.student_rollno[i] = i + 5;
+		}
+		
+		// Printing out the array element
+		for (int i = 0; i < size; i++) {				//Printing index		//Printing the value at that index
+			System.out.println("Student roll at element: " + i + " is " + aa.student_rollno[i]);
+		}
+
+	}
+
+}

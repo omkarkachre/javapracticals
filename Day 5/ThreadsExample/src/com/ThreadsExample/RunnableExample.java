fec5e93bfd7bc54349a8d5a2a4573e0282d60174
omkarkachre <110754398+omkarkachre@users.noreply.github.com>
2022-08-31 10:05:33 +0530

    Add files via upload

--- Day 5/ThreadsExample/src/com/ThreadsExample/RunnableExample.java
+++ Day 5/ThreadsExample/src/com/ThreadsExample/RunnableExample.java
@@ -1,27 +1,18 @@
 package com.ThreadsExample;
 
-public class ThreadsExample extends Thread {
-
+public class RunnableExample implements Runnable {
 	String ThreadName;
-	ThreadsExample(String arg){
+	RunnableExample(String arg){
 		ThreadName = arg;
 	}
 	
 	public void run() {
-		int i = 0;
 		
-		while(i < 1000000000) {
-			System.out.println("Thread \" + ThreadName + \" is saying Hi");
-		}
-		
-		/*
 		try {
 			int a = 10/0;
 
 		}catch(ArithmeticException e) {
 			System.out.println("Number cannot be divided by zero");
-			//e.printStackTrace();
-			//System.out.println(e);
 		}
 		
 		try {
@@ -33,6 +24,6 @@ public class ThreadsExample extends Thread {
 		}catch(Exception e) { //InterruptedException 
 			e.printStackTrace();
 		}
-		*/
+		
 	}
 }
--- Day 5/ThreadsExample/src/com/ThreadsExample/main.java
+++ Day 5/ThreadsExample/src/com/ThreadsExample/main.java
@@ -3,13 +3,28 @@ package com.ThreadsExample;
 public class main {
 
 	public static void main(String args[]) {
+		
+		//When Extending Thread class
 		ThreadsExample t1 = new ThreadsExample("Thread-1");
 		ThreadsExample t2 = new ThreadsExample("Thread-2");
 		ThreadsExample t3 = new ThreadsExample("Thread-3");
+		ThreadsExample t4 = new ThreadsExample("Thread-4");
+		ThreadsExample t5 = new ThreadsExample("Thread-5");
 			
 		t1.start();		// Start can run the run() method automatically
 		t2.start();
 		t3.start();
+		t4.start();
+		t5.start();
+		
+		
+		//When Implementing Runnable Interface
+		RunnableExample obj1 = new RunnableExample("Thread-1");
+		RunnableExample obj2 = new RunnableExample("Thread-2");
+		Thread th1 = new Thread(obj1);
+		Thread th2 = new Thread(obj2);
+		//th1.start();
+		//th2.start();
 	}
 	
 }
